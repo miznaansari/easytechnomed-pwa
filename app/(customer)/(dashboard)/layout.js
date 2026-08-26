cfa38796993167732f743648a5ba5eb5185d81b5
A2D-mohd-mizna-ansari <mohd.ansari@a2deats.com>
2026-08-26 23:36:19 +0530

    f

--- app/(customer)/(dashboard)/layout.js
+++ app/(customer)/(dashboard)/layout.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import AdminLayoutClient from "@/components/AdminLayoutClient";
 import NextTopLoader from "nextjs-toploader";
+
+export const dynamic = "force-dynamic";
 
 export default function AdminDashboardLayout({ children }) {
   return (
     <>
       <NextTopLoader color="#0f766e" showSpinner={false} height={3} />
-      <AdminLayoutClient>
-        {children}
-      </AdminLayoutClient>
+      <Suspense fallback={null}>
+        <AdminLayoutClient>
+          {children}
+        </AdminLayoutClient>
+      </Suspense>
     </>
   );
 }
